a3841c454089aecb9320daed87015442fe5148d4
Lyngsoe <joneslyngsoe@gmail.com>
2019-12-29 13:13:51 +0100

    full rl

---
+++ models/rl_transformer_full.py
@@ -0,0 +1,184 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.transformer import Transformer
+import numpy as np
+from datetime import datetime
+from torch.nn.modules import Linear
+from torch.autograd import Variable
+import math
+
+class RLTransformer:
+
+    def __init__(self, base_path,reward_function,d_model=128,dff=2048,embeddings=None,num_layers=6,output_size=30522,nhead=12,device="gpu",dropout=0.2,lr=0.05,l2=0,exp_name=None):
+        self.base_path = base_path
+        self.save_path = self.base_path + "experiments/"
+        self.device = device
+        self.model_name = "RL_TransformerBERT"
+        self.vocab_size = output_size
+        self.lr = lr
+        self.d_mdodel = d_model
+        self.reward_function = reward_function
+
+        self.exp_name = exp_name if exp_name is not None else self.get_exp_name()
+
+        self.model = Transformer(d_model=d_model,nhead=nhead,num_decoder_layers=num_layers,num_encoder_layers=num_layers,dim_feedforward=dff,dropout=dropout).cuda().double()
+        self.linear_out = Linear(d_model,output_size).cuda().double()
+        self.target_in = nn.Embedding(output_size, d_model, padding_idx=1).cuda().double()
+
+        if embeddings is not None:
+            self.target_in.weight = nn.Parameter(embeddings)
+            self.target_in.weight.requires_grad = False
+
+        self.pos_enc = PositionalEncoding(d_model,dropout).cuda().double()
+
+
+        classW = torch.ones(output_size, device=self.device).double()
+        classW[1] = 0
+
+        self.criterion = nn.CrossEntropyLoss(weight=classW)
+
+        params = list(self.model.parameters()) + list(self.linear_out.parameters()) + list(self.target_in.parameters())
+        self.optimizer = torch.optim.Adam(params, lr=self.lr,weight_decay=l2)
+
+        params = list(self.model.parameters()) + list(self.linear_out.parameters()) + list(self.target_in.parameters())
+        print("#parameters:", sum([np.prod(p.size()) for p in params]))
+
+    def train_predict(self,x,q_mask):
+        #print("x:",x.size())
+        self.model.train()
+        self.target_in.train()
+        self.optimizer.zero_grad()
+        max_len = 20
+
+        m_out = torch.zeros(max_len, x.size(1)).cuda().long()
+        m_out[0] = torch.Tensor(x.size(1), 1).fill_(2).cuda().long()
+
+        lin_in = self.pos_enc(x)
+        x_mask = q_mask == 1
+
+        for i in range(1, max_len):
+            mask = (torch.triu(torch.ones(i, i)) == 1).transpose(0, 1).float()
+            tgt_mask = mask.masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0)).cuda().double()
+            tgt_in = self.pos_enc(self.target_in(m_out[:i]))
+            out = self.model(lin_in, tgt_in, tgt_mask=tgt_mask, src_key_padding_mask=x_mask)
+            m_out[i] = torch.argmax(self.linear_out(out[-1]))
+
+        self.preds = self.linear_out(out)
+        preds_no_grad = self.preds.detach()
+
+        return preds_no_grad.cpu().numpy()
+
+    def predict(self,x,q_mask):
+        self.optimizer.zero_grad()
+        self.model.eval()
+        self.linear_out.eval()
+        self.linear_in.eval()
+        max_len = 20
+
+        m_out = torch.zeros(max_len, x.size(1)).cuda().long()
+        m_out[0] = torch.Tensor(x.size(1), 1).fill_(2).cuda().long()
+
+        lin_in = self.pos_enc(x)
+        x_mask = q_mask == 1
+
+        for i in range(1, max_len):
+            mask = (torch.triu(torch.ones(i, i)) == 1).transpose(0, 1).float()
+            tgt_mask = mask.masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0)).cuda().double()
+            tgt_in = self.pos_enc(self.target_in(m_out[:i]))
+            out = self.model(lin_in, tgt_in, tgt_mask=tgt_mask, src_key_padding_mask=x_mask)
+            m_out[i] = torch.argmax(self.linear_out(out[-1]))
+
+        self.preds = self.linear_out(out)
+        preds_no_grad = self.preds.detach()
+
+        return preds_no_grad.cpu().numpy()
+
+    def calc_reward(self,*args,**kwargs):
+        return self.reward_function(*args,**kwargs)
+
+    def update_policy(self,reward,update = True):
+        # Update network weights
+        reward = Variable(torch.from_numpy(np.array(reward)).double().cuda())
+
+
+        self.preds = torch.softmax(self.preds,dim=2)
+        weights = torch.log(torch.max(self.preds,dim=2)[0])
+        weights = torch.matmul(weights,reward)
+
+        loss = -(torch.mean(weights))
+
+        if update:
+            loss.backward()
+            self.optimizer.step()
+
+        return loss.item()
+
+    def get_exp_name(self):
+        now = datetime.now()
+        return self.model_name+"__"+now.strftime("%m-%d_%H:%M")
+
+    def save_latest(self,epoch):
+        save_path = self.save_path + self.exp_name + "/latest"
+        self._save(epoch,save_path)
+    def save_best(self,epoch):
+        save_path = self.save_path + self.exp_name + "/best"
+        self._save(epoch,save_path)
+
+    def _save(self, epoch, save_path):
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict()
+        }, save_path + "/model.pt")
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.linear_out.state_dict(),
+        }, save_path + "/linear_out.pt")
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.target_in.state_dict(),
+        }, save_path + "/target_in.pt")
+
+    def load(self, save_path, train):
+
+        checkpoint = torch.load(save_path + "/model.pt")
+        self.model.load_state_dict(checkpoint["model_state_dict"])
+        self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        epoch = checkpoint["epoch"]
+
+        checkpoint = torch.load(save_path + "/linear_out.pt")
+        self.linear_out.load_state_dict(checkpoint["model_state_dict"])
+
+        checkpoint = torch.load(save_path + "/target_in.pt")
+        self.target_in.load_state_dict(checkpoint["model_state_dict"])
+
+        if train:
+            self.model.train()
+            self.linear_out.train()
+        else:
+            self.model.eval()
+            self.linear_out.eval()
+
+        return epoch
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
---
+++ run_rl_transformer_full.py
@@ -0,0 +1,78 @@
+from training.rl_trainer import Trainer
+from models.rl_transformer_full import RLTransformer
+from models.losses.recall_reward import RecallRewardMean
+from models.losses.rank_loss import RankReward
+from models.baselines.moving_avg import MovingAverage,MovingStdAverage,MovingQuestionStdAverage
+from search_engine.elastic_search import ELSearch
+import torch
+import jsonlines
+import json
+from tqdm import tqdm
+import numpy as np
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = "cpu"
+
+#base_path = "/home/jonas/data/squad/"
+base_path = "/media/jonas/archive/master/data/rl_squad/"
+#base_path = "/scratch/s134280/squad/"
+
+vocab_size = 30522
+emb_size = 768 # embedding dimension
+d_model = 128 # the dimension of the feedforward network model in nn.TransformerEncoder
+n_layers = 6 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 8 # the number of heads in the multiheadattention models
+dropout = 0.2 # the dropout value
+dff = d_model*4 # dimension of feed forward
+batch_size = 64
+lr = 10e-7
+epochs = 500
+l2 = 0
+
+#base_line = MovingAverage(0.42499358759755385)
+#base_line = MovingStdAverage()
+
+embeddings = np.load("/home/jonas/Documents/master/embedding_method/bert_embeddings.npy")
+embeddings = torch.tensor(embeddings,device=device).double()
+
+base_line = MovingStdAverage()
+reward_function = RankReward(base_line)
+
+specs = {
+    "vocab_size":vocab_size,
+    "emb_size":emb_size,
+    "d_model": d_model,
+    "n_layers": n_layers,
+    "nhead": nhead,
+    "dropout": dropout,
+    "dff": dff,
+    "lr": lr,
+    "l2": l2,
+    "epochs": epochs,
+    "base_line":base_line.name,
+    "reward_function":reward_function.name
+}
+
+
+
+
+search_engine = ELSearch("squad")
+
+load = True
+
+if load:
+    load_path = "/media/jonas/archive/master/data/squad_real/experiments/TransformerBERT__12-23_00:57"
+    model = RLTransformer(base_path, reward_function,embeddings=embeddings,num_layers=n_layers, output_size=vocab_size, device=device,nhead=nhead, dropout=dropout, d_model=d_model, dff=dff, lr=lr,l2=l2)
+    epoch = model.load(load_path +"/latest",train=True)
+    epoch=0
+    specs.update({"load_path":load_path})
+    trainer = Trainer(model=model,search_engine=search_engine,base_path=base_path,batch_size=batch_size,device=device,epoch=epoch,max_epoch=epochs,specs=specs)
+else:
+    model = RLTransformer(base_path, reward_function,embeddings=embeddings, output_size=vocab_size, device=device,nhead=nhead, dropout=dropout, d_model=d_model, dff=dff, lr=lr,l2=l2)
+    trainer = Trainer(model=model,base_path=base_path,search_engine=search_engine,batch_size=batch_size,device=device,max_epoch=epochs,specs=specs)
+
+trainer.train()
+
+
+
